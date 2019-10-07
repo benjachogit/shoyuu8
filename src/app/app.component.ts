0eb716562500a2b13faaad1cfbcdb55a98166385
unknown <benjarat.cp@gmail.com>
2019-10-07 12:56:45 +0700

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,23 +4,24 @@ import xkcd from '../assets/cropimg.json';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: [ `
-    ngui-in-view {
-      min-height: 300px;
-    }
-    .num-images { 
-      position: fixed; padding: 5px;
-      bottom: 0; right: 0; background: #333;color: #fff;
-    }` ]
+  styles: []
+  // styles: [ `
+  //   ngui-in-view {
+  //     min-height: 300px;
+  //   }
+  //   .num-images { 
+  //     position: fixed; padding: 5px;
+  //     bottom: 0; right: 0; background: #333;color: #fff;
+  //   }` ]
 })
 export class AppComponent  {
 
-  mainmenu:any = xkcd;
-  get numImages(): number {
-    return this.element.nativeElement.querySelectorAll('img').length;
-  }
+  // mainmenu:any = xkcd;
+  // get numImages(): number {
+  //   return this.element.nativeElement.querySelectorAll('img').length;
+  // }
   
-  numArr = Array.from(Array(100), (_,x) => x);
+  // numArr = Array.from(Array(100), (_,x) => x);
 
   
 
